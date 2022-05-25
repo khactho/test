8cd5941eabab4435d5616fb5ae59a82ec566c2ec
Nguyễn Khắc Thơ <khactho@MacBook-Pro-cua-Nguyen.local>
2022-05-25 15:53:08 +0700

    ok

---
+++ prj2/prj2App.swift
@@ -0,0 +1,17 @@
+//
+//  prj2App.swift
+//  prj2
+//
+//  Created by Nguyễn Khắc Thơ on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct prj2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
